18cb2963bb94fb6df3ce586287b4a21346513586
Shabbir <2016sabbirahmed@gmail.com>
2022-10-09 11:42:48 +0600

    modify second

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-const h2 = document.createElement("h2");
-console.log(h2);
+const time = () => {
+  const h2 = document.createElement("h2");
+  h2.className = "watch";
+  const date = new Date();
 
-const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
 
-const hour = date.getHours();
-const minute = date.getMinutes();
-const second = date.getSeconds();
+  const hours = hour > 12 ? hour - 12 : hour;
+  const minutes = minute < 10 ? "0" + minute : minute;
+  const seconds = second < 10 ? "0" + second : second;
 
-const hours = hour > 12 ? hour - 12 : hour;
-const minutes = minute < 10 ? "0" + minute : minute;
-const seconds = second < 10 ? "0" + second : second;
+  const watch = `${hours} : ${minutes} : ${seconds}`;
 
-const watch = `${hours} : ${minutes} : ${seconds}`;
-console.log(watch);
-
-const digitalWatch = document.createTextNode(watch);
-h2.appendChild(digitalWatch);
-console.log(h2);
-
-document.body.appendChild(h2);
-
-h2.className = "watch";
+  const digitalWatch = document.createTextNode(watch);
+  h2.appendChild(digitalWatch);
+  const showDigitalWatch = document.body.appendChild(h2);
+  return showDigitalWatch;
+};
+console.log(time());
